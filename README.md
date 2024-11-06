fa7437f4d4323a85e33b0bd918279d7c7334052f
František Jan Maxmilián Antonín Rubáček <100533721+Ferenc1234@users.noreply.github.com>
2024-11-06 10:53:41 +0000

    This code should be faster, so it will not run into issues with multiple letters, it is even possible that it will take any word that contains "windows" and it will change to "linux" and leave the rest of the word. Also updated README.md

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-# Linux-For-Gotham
+# GrussVirus
 
-## Compiling
+This app will change your typed text "Windows" to "Linux" (Just what Grussmann love)
 
-`gcc main.c -o linux.exe -s`
+## How to run the app
 
-> use the `-mwindows` argument for a hidden window
+1. Download or copy `main.c` to your computer and save it somewhere
 
-This is just a joke and isn't meant to cause any harm.
+2. Run this command in windows powershell inside the folder with the file (make sure you have gcc installed) <br>
+`gcc main.c -o linux.exe -s`<br>
+or use <br>
+`gcc main.c -o linux.exe -s -mwindows`<br>
+for hidden window (you probably want the second option).
+
+3. Double click the compiled file (linux.exe) 
+4. Enjoy!
+
+## Making the app run automatically after windows startup
+
+1. Press `Ctrl + R` to open RUN window
+2. Write `shell:Common Startup` into the opened window
+3. Press OK
+4. Now move or copy the linux.exe file into the opened folder (you will need admin acces probably)
+5. Your app should start after startup of your machine!
+
--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-#include <Windows.h>
+#include <Windows.h> //imports windows api
 
-DWORD history[7] = {0, 0, 0, 0, 0, 0, 0};
+DWORD history[7] = {0, 0, 0, 0, 0, 0, 0}; //history of last 7 typed letters
 
-char typed = 0;
+char typed = 0; //if the word "WINDOWS" is typed
 
-void simulate_key_press(DWORD key, char ctrl) {
+void simulate_key_press(DWORD key, char ctrl) { //this will simulate key presses
     INPUT input;
     input.type = INPUT_KEYBOARD;
     input.ki.wScan = MapVirtualKey(key, MAPVK_VK_TO_VSC);
@@ -12,31 +12,31 @@ void simulate_key_press(DWORD key, char ctrl) {
     input.ki.dwExtraInfo = 0;
 
     if (ctrl == 1) {
-        input.ki.wVk = VK_CONTROL;
+        input.ki.wVk = VK_CONTROL; //presses the control key
         input.ki.dwFlags = 0;
         SendInput(1, &input, sizeof(INPUT));
     }
 
     input.ki.wVk = key;
     input.ki.dwFlags = 0;
-    SendInput(1, &input, sizeof(INPUT));
+    SendInput(1, &input, sizeof(INPUT)); //sends the key press
 
-    Sleep(1);
+    Sleep(1); //1 ms pause
 
-    input.ki.dwFlags = KEYEVENTF_KEYUP;
-    SendInput(1, &input, sizeof(INPUT));
+    input.ki.dwFlags = KEYEVENTF_KEYUP; //unpresses the key
+    SendInput(1, &input, sizeof(INPUT)); //sends the key unpress
 
-    if (ctrl == 1) {
+    if (ctrl == 1) { //if parameter ctrl is 1, then press the control key
         input.ki.wVk = VK_CONTROL;
         input.ki.dwFlags = KEYEVENTF_KEYUP;
         SendInput(1, &input, sizeof(INPUT));
     }
 }
 
-LRESULT CALLBACK low_level_keyboard_proc(int n_code, WPARAM w_param, LPARAM l_param) {
+LRESULT CALLBACK low_level_keyboard_proc(int n_code, WPARAM w_param, LPARAM l_param) { //this is the callback function, if WIDNOWS was typed, then it will simulate the key press
     if (typed == 1) {
-        typed = 0;
-        simulate_key_press(VK_BACK, 0);
+        typed = 0; //changes the value of typed to 0 (so windows can be typed again)
+        simulate_key_press(VK_BACK, 0); //deletes last letter
     }
     if (n_code == HC_ACTION) {
         KBDLLHOOKSTRUCT *keyboard = (KBDLLHOOKSTRUCT *)l_param;
@@ -49,24 +49,12 @@ LRESULT CALLBACK low_level_keyboard_proc(int n_code, WPARAM w_param, LPARAM l_pa
                 history[6] = keyboard->vkCode;
 
                 if (history[0] == 'W' && history[1] == 'I' && history[2] == 'N' && history[3] == 'D' && history[4] == 'O' && history[5] == 'W' && history[6] == 'S') {
-                    simulate_key_press('A', 1);
-                    simulate_key_press(VK_BACK, 0);
+                    simulate_key_press(VK_BACK, 1); //press the key backspace and control
                     simulate_key_press('L', 0);
                     simulate_key_press('I', 0);
                     simulate_key_press('N', 0);
                     simulate_key_press('U', 0);
                     simulate_key_press('X', 0);
-                    simulate_key_press(VK_SPACE, 0);
-                    simulate_key_press('F', 0);
-                    simulate_key_press('O', 0);
-                    simulate_key_press('R', 0);
-                    simulate_key_press(VK_SPACE, 0);
-                    simulate_key_press('G', 0);
-                    simulate_key_press('O', 0);
-                    simulate_key_press('T', 0);
-                    simulate_key_press('H', 0);
-                    simulate_key_press('A', 0);
-                    simulate_key_press('M', 0);
                     typed = 1;
                 }
         }
@@ -76,13 +64,13 @@ LRESULT CALLBACK low_level_keyboard_proc(int n_code, WPARAM w_param, LPARAM l_pa
 }
 
 int main() {
-    HHOOK keyboard_hook = SetWindowsHookEx(WH_KEYBOARD_LL, low_level_keyboard_proc, NULL, 0);
+    HHOOK keyboard_hook = SetWindowsHookEx(WH_KEYBOARD_LL, low_level_keyboard_proc, NULL, 0); //sets the hook
     if (keyboard_hook == NULL) {
         return 1;
     }
     
     MSG message;
-    while (GetMessage(&message, NULL, 0, 0)) {
+    while (GetMessage(&message, NULL, 0, 0)) { //this should be for capturing the typed letters (not 100% sure)
         TranslateMessage(&message);
         DispatchMessage(&message);
     }
